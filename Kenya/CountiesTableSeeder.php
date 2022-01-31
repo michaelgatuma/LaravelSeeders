e998d97ec8f604a8cbf1a28b17f39b7d07f04465
Michael Gatuma <50529359+michaelgatuma@users.noreply.github.com>
2022-01-31 17:17:24 +0300

    Update CountiesTableSeeder.php

--- Kenya/CountiesTableSeeder.php
+++ Kenya/CountiesTableSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-/* Laravel 7.* Seeder for 47 Counties in Kenya with their county codes
+/* Laravel 7.x Seeder for 47 Counties in Kenya with their county codes
  * @Author: Michael Gatuma
  * @Date: Jan 31st 2022
  */
